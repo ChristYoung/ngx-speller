ac013fa5f0d6b84a0c806a385c86392dce0232d1
jay.yang <858851768@qq.com>
2024-08-21 15:28:46 +0800

    🔧 build:

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,40 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const typeScriptEsLintPlugin = require('@typescript-eslint/eslint-plugin');
+const esLintConfigPrettier = require('eslint-config-prettier');
+
+// Translate ESLintRC-style configs into flat configs.
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: typeScriptEsLintPlugin.configs['recommended'],
+});
+
 module.exports = {
-    parserOptions: {
-        project: ['./tsconfig.json'],
-        sourceType: 'module',
-    },
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@angular-eslint/recommended',
-        'prettier',
-    ],
-    plugins: [
-        '@typescript-eslint',
-        '@angular-eslint',
-        'prettier',
-    ],
-    rules: {
-        'prettier/prettier': ['error'],
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-        'no-console': 'warn',
-    },
+    ...compat.config({
+        parserOptions: {
+            project: ['./tsconfig.json'],
+            sourceType: 'module',
+        },
+        env: {
+            browser: true,
+            es2021: true,
+        },
+        extends: [
+            // 'eslint:recommended',
+            'plugin:@typescript-eslint/recommended',
+            'plugin:@angular-eslint/recommended',
+            'prettier',
+        ],
+        plugins: [
+            '@typescript-eslint',
+            '@angular-eslint',
+            'prettier',
+        ],
+        rules: {
+            'prettier/prettier': ['error'],
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+            'no-console': 'warn',
+        },
+    }),
+    esLintConfigPrettier,
 };
