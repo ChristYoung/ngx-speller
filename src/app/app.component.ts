d9eff8f379f2e5027800b97897b1aa88dbb0899b
jay.yang <858851768@qq.com>
2024-10-08 21:36:11 +0800

    🐳 chore:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Amplify } from 'aws-amplify';
 import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
-// import outputs from '../../amplify_outputs.json';
+import outputs from '../../amplify_outputs.json';
 
 @Component({
   selector: 'app-root',
@@ -15,31 +15,32 @@ import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/u
 export class AppComponent {
 
   constructor(public authenticator: AuthenticatorService) {
-    Amplify.configure({
-      Auth: {
-        Cognito: {
-          userPoolId: "eu-north-1_ymMp0Epie",
-          userPoolClientId: "22mbh4g7sigd1fkg76l96em3ti",
-          identityPoolId: "eu-north-1:16b5fb3f-aa4e-473e-ae8e-b185c3844ac4",
-          loginWith: {
-            email: true,
-          },
-          signUpVerificationMethod: "code",
-          userAttributes: {
-            email: {
-              required: true,
-            },
-          },
-          allowGuestAccess: true,
-          passwordFormat: {
-            minLength: 8,
-            requireLowercase: true,
-            requireUppercase: true,
-            requireNumbers: true,
-            requireSpecialCharacters: true,
-          },
-        },
-      },
-    });
+    Amplify.configure(outputs);
+    // Amplify.configure({
+    //   Auth: {
+    //     Cognito: {
+    //       userPoolId: "eu-north-1_ymMp0Epie",
+    //       userPoolClientId: "22mbh4g7sigd1fkg76l96em3ti",
+    //       identityPoolId: "eu-north-1:16b5fb3f-aa4e-473e-ae8e-b185c3844ac4",
+    //       loginWith: {
+    //         email: true,
+    //       },
+    //       signUpVerificationMethod: "code",
+    //       userAttributes: {
+    //         email: {
+    //           required: true,
+    //         },
+    //       },
+    //       allowGuestAccess: true,
+    //       passwordFormat: {
+    //         minLength: 8,
+    //         requireLowercase: true,
+    //         requireUppercase: true,
+    //         requireNumbers: true,
+    //         requireSpecialCharacters: true,
+    //       },
+    //     },
+    //   },
+    // });
   }
 }
