d1faf9e174d7ca7fea26dfb1a36986d1bf031f1c
jay.yang <858851768@qq.com>
2024-10-19 10:57:13 +0800

    🐳 chore:

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,1 @@
-export const environment = {
-  production: true,
-  fireBaseConfig: {
-    projectId: 'speller-d44b7',
-    appId: '1:269355986460:web:843364c553f0889cb679d9',
-    storageBucket: 'speller-d44b7.appspot.com',
-    apiKey: 'AIzaSyBDBEO6bzpEqkXPg1jUpk-a2GlBmzxTKrE',
-    authDomain: 'speller-d44b7.firebaseapp.com',
-    messagingSenderId: '269355986460',
-    measurementId: 'G-RM86DDEWD2',
-  },
-};
+export const environment = {};
