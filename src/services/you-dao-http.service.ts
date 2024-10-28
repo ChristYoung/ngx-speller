726ff5517485d7604c090f8c5e6f77fae1479523
jay.yang <858851768@qq.com>
2024-10-28 21:06:43 +0800

    🐳 chore:

--- src/services/you-dao-http.service.ts
+++ src/services/you-dao-http.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { WORDS_COMPLEX_EXPLANATION } from '../core/constant';
 import { map, Observable } from 'rxjs';
+import { FREE_DICTIONARY_API } from '../core/constant';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +10,7 @@ export class YouDaoHttpService {
   constructor(private http: HttpClient) {}
 
   getYouDaoWordItemByHttp(wordStr: string): Observable<any> {
-    return this.http.get<any>(`${WORDS_COMPLEX_EXPLANATION}/${wordStr}`).pipe(
+    return this.http.get<any>(`${FREE_DICTIONARY_API}/${wordStr}`).pipe(
       map((res) => {
         res.word = wordStr;
         return res;
