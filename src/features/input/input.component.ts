d853fe79c1110e27c265cf408529a76196eb4c22
jay.yang <858851768@qq.com>
2024-10-29 21:34:12 +0800

    🌈 style: catch input word error.

--- src/features/input/input.component.ts
+++ src/features/input/input.component.ts
@@ -3,9 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { DbService } from '../../services/DataBase/db.service';
 import { ZorroModule } from '../../zorro/zorro.module';
 import { Store } from '@ngrx/store';
-import { finalize, Observable, switchMap } from 'rxjs';
+import { catchError, EMPTY, finalize, Observable, switchMap } from 'rxjs';
 import { Settings } from '../../types';
 import { CommonModule } from '@angular/common';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-input',
@@ -39,6 +40,7 @@ import { CommonModule } from '@angular/common';
 export class InputComponent {
   private db = inject(DbService);
   private store = inject(Store);
+  private nzMessage = inject(NzMessageService);
   setting$: Observable<Settings>;
 
   inputWords: string = '';
@@ -57,6 +59,12 @@ export class InputComponent {
           return this.db.addWordsToIndexDBByInput(this.inputWords, apiType);
         }),
         finalize(() => (this.loading = false)),
+        catchError((e) => {
+          const errorMessage =
+            e?.target?.error?.message ?? 'Input word error, please try again later.';
+          this.nzMessage.error(errorMessage);
+          return EMPTY;
+        }),
       )
       .subscribe(() => (this.inputWords = '')); // clear input after submit
   }
