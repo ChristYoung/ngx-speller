b950380cbb3494a7736f07dc7d4f749a2b183524
jay.yang <858851768@qq.com>
2024-10-30 14:31:55 +0800

    🎉 init: create typing quiz card component.

---
+++ src/features/spelling/typing-quiz/typing-quiz.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-typing-quiz',
+  standalone: true,
+  imports: [],
+  template: ` <p>typing-quiz works!</p> `,
+  styleUrl: './typing-quiz.component.less',
+})
+export class TypingQuizComponent {}
