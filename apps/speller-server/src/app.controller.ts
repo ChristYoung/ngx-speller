80bcca7965f3a2c8030e57126680310cebc4b0fb
jay.yang <858851768@qq.com>
2024-12-02 19:25:37 +0800

    🎉 start the nestjs app.

--- apps/speller-server/src/app.controller.ts
+++ apps/speller-server/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('explanations/:word')
+  getExplanations(@Param('word') word: string): Promise<any> {
+    return firstValueFrom(this.appService.getExplanations(word));
   }
 }
--- apps/speller-server/src/app.module.ts
+++ apps/speller-server/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BASE_WORD_API } from './constant';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
+  imports: [
+    HttpModule.register({
+      baseURL: BASE_WORD_API,
+      timeout: 5000,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
