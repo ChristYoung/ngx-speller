c539c51a02ed6529217dd8fb67a3bdd5e31b1f51
jay.yang <858851768@qq.com>
2024-12-07 14:58:15 +0800

    update

--- apps/ngx-speller-fe/src/services/Azure/azure-speech.service.ts
+++ apps/ngx-speller-fe/src/services/Azure/azure-speech.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 // import * as SpeechSDK from 'microsoft-cognitiveservices-speech-sdk';
 
 declare const SpeechSDK: any;
@@ -10,17 +11,15 @@ export class AzureSpeechService {
   private readonly subscriptionKey = import.meta.env['NG_APP_AZURE'];
   private readonly region = import.meta.env['NG_APP_REGION'];
 
+  synthesizer: any;
+
   constructor() {}
 
   speak(sentence: string): void {
-    SpeechSDK.SpeechRecognizer.enableNativeAudio = false;
     const speechConfig = SpeechSDK.SpeechConfig.fromSubscription(this.subscriptionKey, this.region);
-    speechConfig.speechSynthesisVoiceName = 'en-US-AriaNeural';
-    const audioConfig = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
+    this.synthesizer = new SpeechSDK.SpeechSynthesizer(speechConfig);
 
-    const synthesizer = new SpeechSDK.SpeechSynthesizer(speechConfig, audioConfig);
-
-    synthesizer.speakTextAsync(
+    this.synthesizer.speakTextAsync(
       sentence,
       (result) => {
         if (result.reason === SpeechSDK.ResultReason.SynthesizingAudioCompleted) {
@@ -28,12 +27,53 @@ export class AzureSpeechService {
         } else {
           console.error('Speech synthesis failed:', result.errorDetails);
         }
-        synthesizer.close();
+        this.synthesizer.close();
+        this.synthesizer = undefined;
       },
       (error) => {
         console.error('Error during synthesis:', error);
-        synthesizer.close();
+        this.synthesizer.close();
+        this.synthesizer = undefined;
       },
     );
+  }
+
+  speakText(text: string): Observable<any> {
+    return new Observable((observer) => {
+      const speechConfig = SpeechSDK.SpeechConfig.fromSubscription(
+        this.subscriptionKey,
+        this.region,
+      );
+      this.synthesizer = new SpeechSDK.SpeechSynthesizer(speechConfig);
+
+      this.synthesizer.speakTextAsync(
+        text,
+        (result) => {
+          if (result.reason === SpeechSDK.ResultReason.SynthesizingAudioCompleted) {
+            console.log('Synthesis finished.');
+            observer.next(result);
+            observer.complete();
+          } else {
+            console.error('Speech synthesis failed:', result.errorDetails);
+            observer.error(result.errorDetails);
+          }
+          this.synthesizer.close();
+          this.synthesizer = undefined;
+        },
+        (error) => {
+          console.error('Error during synthesis:', error);
+          observer.error(error);
+          this.synthesizer.close();
+          this.synthesizer = undefined;
+        },
+      );
+    });
+  }
+
+  destroySpeaker(): void {
+    if (this.synthesizer) {
+      this.synthesizer.close();
+      this.synthesizer = undefined;
+    }
   }
 }
--- apps/ngx-speller-fe/src/widgets/speech/speech.component.ts
+++ apps/ngx-speller-fe/src/widgets/speech/speech.component.ts
@@ -1,14 +1,20 @@
-import { Component, inject, Input } from '@angular/core';
-import { PreventButtonDefaultDirective } from '../../directives/prevent-button-default.directive';
-import { ZorroModule } from '../../zorro/zorro.module';
+import { ChangeDetectorRef, Component, inject, Input } from '@angular/core';
 import { AzureSpeechService } from '../../services/Azure/azure-speech.service';
+import { ZorroModule } from '../../zorro/zorro.module';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-speech',
   standalone: true,
-  imports: [ZorroModule, PreventButtonDefaultDirective],
+  imports: [ZorroModule],
   template: `
-    <button nz-button (click)="triggerSpeech(speechText)" nzType="default" nzShape="circle">
+    <button
+      nz-button
+      (click)="triggerSpeech(speechText)"
+      [nzLoading]="loading"
+      nzType="default"
+      nzShape="circle"
+    >
       <span nz-icon nzType="sp:sound" nzTheme="outline"></span>
     </button>
   `,
@@ -17,8 +23,21 @@ import { AzureSpeechService } from '../../services/Azure/azure-speech.service';
 export class SpeechComponent {
   @Input({ required: true }) speechText: string = '';
   speechService = inject(AzureSpeechService);
+  _cd = inject(ChangeDetectorRef);
+  loading: boolean = false;
 
   triggerSpeech(sentence: string): void {
-    this.speechService.speak(sentence);
+    this.loading = true;
+    this.speechService
+      .speakText(sentence)
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+          this._cd.markForCheck();
+        }),
+      )
+      .subscribe(() => {
+        console.log('finished speak');
+      });
   }
 }
