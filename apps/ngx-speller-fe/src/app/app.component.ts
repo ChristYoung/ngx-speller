54b9b624fc4c7ec0f192c9ed289a244a342037b2
jay.yang <858851768@qq.com>
2024-12-25 10:50:53 +0800

    🐳 chore:

--- apps/ngx-speller-fe/src/app/app.component.ts
+++ apps/ngx-speller-fe/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
 })
