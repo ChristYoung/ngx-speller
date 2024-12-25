879a91faddd019f95a06a29c4f6c5d5fb3c238e6
jay.yang <858851768@qq.com>
2024-12-25 17:17:43 +0800

    🦄 refactor:

--- apps/ngx-speller-fe/src/core/constant.ts
+++ apps/ngx-speller-fe/src/core/constant.ts
@@ -2,14 +2,22 @@ import { CommonSettingsConfig, FiltersConfig, Settings } from '../types';
 
 export const BASE_API = 'https://ngx-speller-server.onrender.com';
 export const AUDIO_SRC = 'https://dict.youdao.com/dictvoice?type=0&audio=';
-export const WORDS_SIMPLE_SUGGESTIONS = '/suggest?num=1&ver=3.0&doctype=json&cache=false&le=en&q='; // 单词联想, 输入生词的时候使用
-export const WORDS_COMPLEX_EXPLANATION = `${BASE_API}/explanations`; // 获取单词的释义， 音标, 和例句等
-export const FREE_DICTIONARY_API = 'https://api.dictionaryapi.dev/api/v2/entries/en'; //  免费的字典API, 支持跨域访问, 但是不会返回中文释义, // https://dictionaryapi.dev/
-export const YOU_DAO_API = 'https://dict.youdao.com/jsonapi?jsonversion=2&client=mobile&q='; // 有道词典的API
-export const DEFAULT_FILTER_LESS_THAN = 9999; // 默认过滤拼写次数小于9999的单词
-export const DEFAULT_RANDOM_PICK_COUNT = 30; // 默认随机抽取30个单词
+export const WORDS_SIMPLE_SUGGESTIONS = '/suggest?num=1&ver=3.0&doctype=json&cache=false&le=en&q='; // Word association, used when entering new words
+export const WORDS_COMPLEX_EXPLANATION = `${BASE_API}/explanations`; // Get the word's definition, phonetic symbols, and examples
+export const FREE_DICTIONARY_API = 'https://api.dictionaryapi.dev/api/v2/entries/en'; //  Free dictionary API, supports cross-domain access, but does not return Chinese definitions, //https://dictionaryapi.dev/
+export const YOU_DAO_API = 'https://dict.youdao.com/jsonapi?jsonversion=2&client=mobile&q='; // Youdao Dictionary API
+export const DEFAULT_FILTER_LESS_THAN = 9999; // By default, words spelled less than 9999 times are filtered
+export const DEFAULT_RANDOM_PICK_COUNT = 30; // By default, 30 words are randomly selected
 export const DEFAULT_VOICE_NAME =
   'Microsoft Server Speech Text to Speech Voice (en-US, JennyNeural)';
+
+export const AUTH0_CONFIG = {
+  domain: import.meta.env['NG_APP_AUTH_DOMAIN'],
+  clientId: import.meta.env['NG_APP_AUTH_CLIENT_ID'],
+  authorizationParams: {
+    redirect_uri: window.location.origin,
+  },
+} as const;
 
 export const getDefaultSettings = (allWordsLen: number): Settings => {
   const filters: FiltersConfig = {
