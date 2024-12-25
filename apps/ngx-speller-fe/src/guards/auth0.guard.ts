2a8f45aea43769c4d26576a316c70add381be534
jay.yang <858851768@qq.com>
2024-12-25 21:13:33 +0800

    🐞 fix: avoid the page refresh twice.

--- apps/ngx-speller-fe/src/guards/auth0.guard.ts
+++ apps/ngx-speller-fe/src/guards/auth0.guard.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { CanActivateChild } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
 import { removePreloader } from '../utils';
 
 @Injectable({
@@ -13,20 +13,21 @@ export class Auth0Guard implements CanActivateChild {
 
   canActivateChild(): Observable<boolean> {
     return this.auth0Service.isAuthenticated$.pipe(
-      tap((loggedIn) => {
+      mergeMap((loggedIn) => {
+        console.log('loggedIn', loggedIn);
         if (!loggedIn) {
-          // TODO: Currently, the approach involves waiting until the page is fully loaded and then using a routing guard to determine the redirection,
-          // which results in the page loading twice. While this is a temporary solution,
-          // we plan to optimize this behavior in future updates.
-          // Because loggedIn will be false every time the page is refreshed,
-          // even if you have just logged in before and refreshed again,
-          // it will still become false, so you need to be redirected to log in again,
-          // which leads to the problem of refreshing twice.
-          this.auth0Service.loginWithRedirect().subscribe(() => {
-            removePreloader();
-          });
+          // To avoid the page refresh twice,
+          // we use the getAccessTokenSilently() method to get the access token silently instead of using `loginWithRedirect`.
+          // If the access token is available, the user is logged in and the page can be loaded normally.
+          return this.auth0Service.getAccessTokenSilently().pipe(
+            map(() => {
+              removePreloader();
+              return true;
+            }),
+          );
         } else {
           removePreloader();
+          return of(true);
         }
       }),
     );
